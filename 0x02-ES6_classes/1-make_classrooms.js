c1b582d6ab0d46e323bd7702d4cd50fa3c0d53fd
abraham-ship <abuzsein3@gmail.com>
2023-11-30 20:25:51 +0000

    eslint fix

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,6 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-	const size = [19, 20, 34];
-	return size.map(size => new ClassRoom(size));
+  const size = [19, 20, 34];
+  return size.map((size) => new ClassRoom(size));
 }
